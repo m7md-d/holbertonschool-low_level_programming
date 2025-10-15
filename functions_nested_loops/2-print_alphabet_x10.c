53309547ecbbe9b7a10308a1a951e9cfa96405da
m7md-d <wazrahp@gmail.com>
2025-10-15 11:23:45 +0000

    add the 2th code

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints the lowercase alphabet followed by a newline 10 times.
+ * print_alphabet - Prints the lowercase alphabet followed by a newline 10x.
  *
- * Return: void
+ * Return: void.
  */
 
 void print_alphabet_x10(void)
 {
 	char alpha = 'a';
 	int i;
+
 	for (i = 0; i < 10; i++)
 	{
 		while (alpha <= 'z')

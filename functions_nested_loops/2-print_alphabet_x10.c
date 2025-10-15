29149458d10ab50cf23c24ac901c2440ec32acd1
m7md-d <wazrahp@gmail.com>
2025-10-15 11:18:35 +0000

    add the 2th code

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the lowercase alphabet followed by a newline 10 times.
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char alpha = 'a';
+	int i;
+	for (i = 0; i <= 10; i++)
+	{
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+		_putchar('\n');
+		alpha = 'a';
+	}
+}

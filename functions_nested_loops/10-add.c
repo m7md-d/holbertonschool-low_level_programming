37b5e530d25677f7455f4229979438383ebb29af
m7md-d <wazrahp@gmail.com>
2025-10-16 21:23:00 +0300

    add the 10th code

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers.
+ * @x: The first integer to be added.
+ * @y: The second integer to be added.
+ *
+ * Return: The sum of the two integers (x + y).
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}

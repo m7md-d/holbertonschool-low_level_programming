c5fd797b0bec4cc9c058ddc3c0ecb2d7940004d7
Dark-Ghost1 <wazrahm@gmail.com>
2025-10-20 13:39:57 +0300

    add the 4th code

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers - Prints the numbers from 0 to 9 except 2 and 4,
+ * print_most_numbers - Prints the numbers from 0 to 9 except 2 and 4,
  * followed by a new line.
  *
  * Return: void

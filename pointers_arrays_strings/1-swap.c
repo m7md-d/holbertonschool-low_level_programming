cbfff45ab20ae6f8f50c6058ff86d9e0a182ee1a
Dark-Ghost1 <wazrahm@gmail.com>
2025-10-21 14:27:14 +0300

    fix Betty-style

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -3,7 +3,7 @@
  * @a: A pointer to the first integer.
  * @b: A pointer to the second integer.
  *
- * Description: Uses a temporary variable to exchange the values 
+ * Description: Uses a temporary variable to exchange the values
  * pointed to by the two pointers.
  * Return: void
  */

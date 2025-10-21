9fc4a8dfe485c8a371468a5ea9784b89feabb489
Dark-Ghost1 <wazrahm@gmail.com>
2025-10-21 14:15:10 +0300

    add Bitty-doc

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,11 @@
-
-
+/**
+ * reset_to_98 - Updates the value pointed to by a pointer to 98.
+ * @n: A pointer to an integer that needs to be reset.
+ *
+ * Description: Takes a pointer to an int as parameter and updates
+ * the value it points to to 98.
+ * Return: void
+ */
 void reset_to_98(int *n)
 {
 	*n = 98;

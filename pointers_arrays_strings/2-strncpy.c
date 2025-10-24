52733bae94667ff2b574f5d486b972119d4f0a3f
m7md-d <wazrahp@gmail.com>
2025-10-24 17:00:57 +0300

    add the 2th code v2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+/**
+ * _strncpy - Copies at most n bytes from a source string to a
+ * destination buffer. If the length of src is less than n, the remainder
+ * of dest is padded with null bytes.
+ * @dest: The destination buffer where the string will be copied.
+ * @src: The source string to be copied.
+ * @n: The maximum number of bytes to copy.
+ *
+ * Return: A pointer to the destination string (dest).
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (n != 0)
+	{
+		if (src[i] != '\0')
+		{
+			dest[i] = src[i];
+		}
+		else
+		{
+			dest[i] = '\0';
+		}
+		i++;
+		n--;
+	}
+	return (dest);
+}

43de3a2f637794c1a23242248fe7e80109a6a9b9
m7md-d <wazrahp@gmail.com>
2025-10-26 00:00:17 +0300

    add the 101 code

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer using _putchar
+ * @n: integer to print
+ *
+ * Description: This function prints any integer, positive
+ * or negative, character by character, using only _putchar.
+ */
+void print_number(int n)
+{
+	int num;
+	int i = 1;
+	int tmp = 0;
+	int j = 1;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		_putchar('\n');
+		return;
+	}
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	num = n;
+
+	while (num / 10 != 0)
+	{
+		i++;
+		num /= 10;
+	}
+
+	while (i > 0)
+	{
+		tmp = i;
+		while (tmp > 1)
+		{
+			j *= 10;
+			tmp--;
+		}
+		_putchar((n / j) + '0');
+		n %= j;
+		i--;
+		j = 1;
+	}
+	_putchar('\n');
+}

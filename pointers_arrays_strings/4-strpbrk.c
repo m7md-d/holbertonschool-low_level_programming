2a3008d9efaf4ab45e63a97bacf9cd8fba8ad46c
Dark-Ghost1 <wazrahm@gmail.com>
2025-10-27 11:34:01 +0300

    add the 4th code v3

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,19 @@
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0, j = 0;
+
+	while (s[i] != '\0')
+	{
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + i);
+			}
+			j++;
+		}
+		i++;
+		j = 0;
+	}
+	return (0);
+}

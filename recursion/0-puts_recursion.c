4660724f60cbe4fb4473b7a94a234db5c5e3ccec
Dark-Ghost1 <wazrahm@gmail.com>
2025-10-27 14:45:38 +0300

    add the first code

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -8,12 +8,13 @@
  */
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	while (s[i] != '\0')
+	if (*s != '\0')
 	{
-		_putchar(s[i]);
-		i++;
+		_putchar(*s);
+		_puts_recursion(++s);
 	}
-	_putchar('\n');
+	else
+	{
+		_putchar('\n');
+	}
 }

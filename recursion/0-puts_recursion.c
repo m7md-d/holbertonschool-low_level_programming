6230673a170f7747c6ad43d6310556e9d5455377
Dark-Ghost1 <wazrahm@gmail.com>
2025-10-27 14:41:22 +0300

    add the first code

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * _puts_recursion - Prints a string followed by a new line using recursion.
+ * @s: The string to be printed.
+ *
+ * Return: void
+ */
 void _puts_recursion(char *s)
 {
 	int i = 0;

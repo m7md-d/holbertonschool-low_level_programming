a0d119087f99a4664481a6d5141f995d423c8ced
Dark-Ghost1 <wazrahm@gmail.com>
2025-10-28 16:47:58 +0300

    fix betty

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -15,14 +15,14 @@ int main(int argc, char **argv)
 
 	if (argc != 3)
 	{
-		puts("Error");
+		puts ("Error");
 		return (1);
 	}
-	
+
 	n1 = atoi(argv[1]);
 	n2 = atoi(argv[2]);
 
 	printf("%d\n", n1 * n2);
 
-	return(0);
+	return (0);
 }

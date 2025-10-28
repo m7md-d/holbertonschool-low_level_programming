7cf238672d1e5732988962e521d9147234be3943
Dark-Ghost1 <wazrahm@gmail.com>
2025-10-28 12:17:37 +0300

    add the 4th code

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,28 @@
+/**
+ * _pow_recursion - Returns the value of x raised to the power of y.
+ * The function uses recursion.
+ * @x: The base number.
+ * @y: The exponent.
+ *
+ * Return: The result of x^y.
+ * Returns -1 if y is negative (error case).
+ * Returns 1 if y is 0.
+ */
+int _pow_recursion(int x, int y)
+{
+	int rem;
+
+	if (y > 0)
+	{
+		rem = y - 1;
+		return(x * _pow_recursion(x, rem));
+	}
+	else if (y < 0)
+	{
+		return(-1);
+	}
+	else
+	{
+		return (1);
+	}
+}

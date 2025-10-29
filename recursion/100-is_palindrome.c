9fb4bcbbf47f0d7ca03f27631234a24426983ecb
Dark-Ghost1 <wazrahm@gmail.com>
2025-10-29 12:45:28 +0300

    add the 7th code

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -1,20 +1,5 @@
-/**
- * pal_helper - checks recursively if string is palindrome
- * @sss: pointer to string
- * @j: starting index
- * @e: ending index
- *
- * Return: 1 if palindrome, 0 otherwise
- */
 int pal_helper(char *sss, int j, int e);
 
-/**
- * pal_end - returns the last index of string
- * @ss: pointer to string
- * @i: current index
- *
- * Return: index of last character (not '\0')
- */
 int pal_end(char *ss, int i);
 
 /**
@@ -32,20 +17,35 @@ int is_palindrome(char *s)
 	return (pal_helper(s, 0, i));
 }
 
+/**
+ * pal_end - finds the last valid character index
+ * @ss: pointer to string
+ * @i: index counter
+ *
+ * Return: last valid index before '\0'
+ */
 int pal_end(char *ss, int i)
 {
 	if (ss[i] != '\0')
 	{
-		return(pal_end(ss, ++i));
+		return (pal_end(ss, ++i));
 	}
 	return (--i);
 }
 
+/**
+ * pal_helper - recursive function to check palindrome
+ * @sss: pointer to string
+ * @j: starting index
+ * @e: ending index
+ *
+ * Return: 1 if palindrome, 0 otherwise
+ */
 int pal_helper(char *sss, int j, int e)
 {
 	if (sss[j] == sss[e] && j < e)
 	{
-		return(pal_helper(sss, ++j, --e));
+		return (pal_helper(sss, ++j, --e));
 	}
 	else if (j >= e)
 	{

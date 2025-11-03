cb6f32b8e420fe4081542c070caef6bf9923f432
Dark-Ghost1 <wazrahm@gmail.com>
2025-11-03 16:28:44 +0300

    Add the 4th code

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+/**
+ * free_grid - Frees a 2-dimensional grid.
+ * @grid: The address of the two dimensional grid.
+ * @height: The height of the grid (rows).
+ *
+ * Description: This function iterates through and frees the memory
+ * for each row pointer first, then frees the main grid pointer.
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+
+	free(grid);
+}

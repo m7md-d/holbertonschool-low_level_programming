d1dbae81dd037db3feb12ea02d6217e080941c5b
Dark-Ghost1 <wazrahm@gmail.com>
2025-11-04 14:16:42 +0300

    Add the 1th code

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -27,10 +27,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n > n2)
 		n = n2;
 
-	p = malloc(sizeof(char) *(i + n + 1));
+	p = malloc(sizeof(char) * (i + n + 1));
 
 	if (p == NULL)
-		exit (98);
+		exit(98);
 	for (i = 0; s1[i] != '\0'; i++)
 		p[i] = s1[i];
 	j = 0;

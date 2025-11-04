ed6d87f3e3b7f0dfb1c478e85cfcb2efe56ac278
Dark-Ghost1 <wazrahm@gmail.com>
2025-11-04 13:06:04 +0300

    Add the first code

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Allocates memory using malloc
+ * @b: number of bytes to allocate
+ *
+ * Return: pointer to allocated memory
+ * exits with status 98 if malloc fails
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+		exit(98);
+	return (p);
+}

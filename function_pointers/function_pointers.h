367e7045b5ecb1473517f8cc0da62212409a6f4e
Dark-Ghost1 <wazrahm@gmail.com>
2025-11-11 11:01:19 +0300

    fix function_pointers.h

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,8 +1,24 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * _putchar - writes a character to stdout
+ * @c: character to print
+ *
+ * Return: On success 1, or -1 on error.
+ */
 int _putchar(char c);
+
+
+/**
+ * print_name - prints a name using a function pointer
+ * @name: string to be printed
+ * @f: pointer to a function that takes a string as argument
+ *
+ * Description: Calls the function pointed to by @f, passing @name to it.
+ */
 void print_name(char *name, void (*f)(char *));
+
 /**
  * void array_iterator(int *array, size_t size, void (*action)(int));
  * int int_index(int *array, int size, int (*cmp)(int));

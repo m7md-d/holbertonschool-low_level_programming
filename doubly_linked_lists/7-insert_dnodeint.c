32744f299e24d66b92bfdd2b3467759183e8f426
Dark-Ghost1 <wazrahm@gmail.com>
2025-11-26 14:05:20 +0300

    Add the 7th code.

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -2,42 +2,41 @@
 #include <stdlib.h>
 
 /**
- * insert_dnodeint_at_index - Inserts a new node at a given position in a list.
+ * insert_dnodeint_at_index - Inserts a new node at a given position.
  * @h: A double pointer to the head of the dlistint_t list.
- * @idx: The index where the new node should be added (starts at 0).
- * @n: The integer data (n) for the new node.
+ * @idx: The index of the list where the new node should be added.
+ *       Index starts at 0.
+ * @n: The integer data for the new node.
  *
  * Return: The address of the new node, or NULL if it failed.
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	dlistint_t *tmp = *h, *new;
 	unsigned int i;
-	dlistint_t *tmp, *new;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	for (i = 0; i < idx && tmp; i++)
+		tmp = tmp->next;
+
+	if (!tmp && idx > i)
+		return (NULL);
+
+	if (!tmp && idx == i)
+		return (add_dnodeint_end(h, n));
 
 	new = malloc(sizeof(dlistint_t));
-
 	if (!new)
 		return (NULL);
-	if (!h)
-		return (NULL);
-	tmp = *h;
+
+	new->prev = tmp->prev;
+	new->next = tmp;
 	new->n = n;
-	for (i = 0; tmp; i++)
-	{
-		if (i == idx)
-		{
-			if (tmp)
-			{
-				new->next = tmp;
-				new->prev = tmp->prev;
-				tmp->prev->next = new;
-				return (new);
-			}
-			free(new);
-			return (NULL);
-		}
-		tmp = tmp->next;
-	}
-	free(new);
-	return (NULL);
+	if (tmp->prev)
+		tmp->prev->next = new;
+	tmp->prev = new;
+
+	return (*h);
 }

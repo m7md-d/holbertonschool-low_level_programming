595c4e3bf97ec129810fa67242aae7a1717ff8ee
Dark-Ghost1 <wazrahm@gmail.com>
2025-11-26 16:07:46 +0300

    Add the 8th code.

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -30,16 +30,12 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		free(tmp);
 		return (1);
 	}
-	if (!tmp && index == i)
-	{
-		if (tmp->prev)
-			tmp->prev->next = NULL;
-		free(tmp);
-		return (1);
-	}
+	if (!tmp)
+		return (-1);
 
 	tmp->prev->next = tmp->next;
-	tmp->next->prev = tmp->prev;
+	if(tmp->next)
+		tmp->next->prev = tmp->prev;
 	free(tmp);
 
 	return (1);

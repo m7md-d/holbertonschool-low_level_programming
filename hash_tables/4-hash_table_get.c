dbeea8e05c69989744ba1f4580b4f66be2279055
Dark-Ghost1 <wazrahm@gmail.com>
2025-12-08 14:04:03 +0300

    Add the 4th code.

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+#include <string.h>
+
+/**
+ * hash_table_get - Retrieves the value associated with a key in a hash table.
+ * @ht: A pointer to the hash table structure.
+ * @key: The key string to look up.
+ *
+ * Return: The value associated with the element
+ * , or NULL if key couldn't be found.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *tmp;
+	unsigned long int i;
+
+	i = key_index((const unsigned char *)key, ht->size);
+
+	tmp = ht->array[i];
+
+	while (tmp)
+	{
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
+	}
+	return (NULL);
+}

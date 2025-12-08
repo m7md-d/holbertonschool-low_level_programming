590fe358fc699c09763d2ec9eb8eb62c3b5d7fd7
Dark-Ghost1 <wazrahm@gmail.com>
2025-12-08 14:06:10 +0300

    Add the 4th code.

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -14,6 +14,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *tmp;
 	unsigned long int i;
 
+	if (ht == NULL)
+		return (NULL);
+
 	i = key_index((const unsigned char *)key, ht->size);
 
 	tmp = ht->array[i];
